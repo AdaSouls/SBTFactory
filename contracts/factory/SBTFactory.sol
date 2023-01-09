5d02ea5dfa1bfa006fa17cf9aa29f02f530dcd58
Matias Falcone <matias.falcone@gmail.com>
2023-01-09 21:23:21 +1100

    added fee and fee address

--- contracts/factory/SBTFactory.sol
+++ contracts/factory/SBTFactory.sol
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "../tokens/ERC5192.sol";
 
-contract SBTFactory {
+contract SBTFactory is Ownable {
+  
+  // Contract Variables
+  uint256 public fee;
+  address public feeAddress;
+
+  // Token Arrays
   ERC5192[] public ERC5192Array;
+  
+
+  constructor(address _feeAddress, uint256 _fee) {
+    feeAddress = _feeAddress;
+    fee = _fee;
+  }
 
   function CreateERC5192(string memory _tokenName, string memory _tokenSymbol, bool _owner) public returns (address) {
     require(bytes(_tokenName).length != 0, "SBTFactory: token name is empty");
